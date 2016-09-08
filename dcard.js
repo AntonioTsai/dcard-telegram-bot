b81f9185c26aa00f8da60a66b567d10645233aae
Antonio Tsai <jimmyjack7777@gmail.com>
2016-09-08 22:10:39 +0800

    Set regExp & headers as global variable

--- dcard.js
+++ dcard.js
@@ -8,27 +8,27 @@ const url = 'https://www.dcard.tw';
 const email = '';
 const pwd = '';
 
+var regExp = /csrfToken":"[^"]+/g;
+var headers = {
+    cookie: '',
+    'content-type': 'application/json',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36',
+    'x-csrf-token' : ''
+};
+
 request(url + '/login', (error, response, body) => {
     if(!error && response.statusCode == 200) {
         let $ = cheerio.load(body);
-        let regExp = /csrfToken":"[^"]+/g;
-        let csrfToken = $('script').text().match(regExp)[0].split('"').slice(-1)[0];
-        let cookies = '';
+        headers['x-csrf-token'] = $('script').text().match(regExp)[0].split('"').slice(-1)[0];
         response.headers['set-cookie'].forEach(d => {
-           cookies += d.split(';')[0] + '; ';
+           headers.cookie += d.split(';')[0] + '; ';
         });
 
-        //console.log(csrfToken);
-        //console.log(cookies);
-
+        console.log('CSRF-Token', headers['x-csrf-token']);
+        console.log('Cookie', headers['cookie']);
         let options = {
             url: url + '/_api/sessions',
-            headers: {
-                cookie: cookies,
-                'content-type': 'application/json',
-                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36',
-                'x-csrf-token': csrfToken
-            },
+            headers: headers,
             json: true,
             body: {
                 email: email,
