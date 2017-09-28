766ed1b70edd70524d43317d0703fa1c052bc781
Antonio Tsai <jimmyjack7777@gmail.com>
2017-09-29 02:03:04 +0800

    Complete function getCSRFToken

--- dcard.js
+++ dcard.js
@@ -11,3 +11,16 @@ exports.getDcard;
 
 // Send invitation request to Dcard
 exports.sendInvitation;
+
+
+const getCSRFToken = () => {
+    return new Promise((resolve, reject) => {
+        // Send request to /_ping to get x-csrf-token
+        rq.get('/_ping', (error, response, body) => {
+            if (!error) {
+                return resolve(response.headers['x-csrf-token']);
+            }
+            return reject(error);
+        });
+    });
+}
