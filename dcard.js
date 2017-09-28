05a57d708ffad60a7b560bece25e1de55ff44140
Antonio Tsai <jimmyjack7777@gmail.com>
2017-09-29 02:37:05 +0800

    Add User-Agent to request.defaults

--- dcard.js
+++ dcard.js
@@ -3,6 +3,9 @@ const config = require('./config.js');
 const request = require('request');
 var rq = request.defaults({
     baseUrl: 'https://www.dcard.tw/_api/',
+    headers: {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+    },
     jar: true
 });
 
