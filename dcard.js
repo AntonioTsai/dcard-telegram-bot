6238c850b97b56455949afbc1b93fb9bd317f628
Antonio Tsai <jimmyjack7777@gmail.com>
2017-09-28 15:02:11 +0800

    Add dcard.js

---
+++ dcard.js
@@ -0,0 +1,9 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const config = require('./config.js');
+
+// Return today's Dcard infomation
+exports.getDcard;
+
+// Send invitation request to Dcard
+exports.sendInvitation;
